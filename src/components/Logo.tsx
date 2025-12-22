3be0eb100db048da008db33b0551552fc05826d3
RunawayGOD <20022002utkarsh@gmail.com>
2025-12-23 03:01:01 +0530

    changed website name

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import svgPaths from "../imports/svg-kgk8e7ds24";
 export function Logo() {
   return (
     <div className="h-[47px] w-[112.686px] font-bold text-[26px]">
-      Comapre Ui
+      Compare Ui
     </div>
   );
 }
