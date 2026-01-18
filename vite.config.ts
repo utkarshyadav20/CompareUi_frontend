7608a617de06b174e9794b87387b27e16b2d1af4
RunawayGOD <20022002utkarsh@gmail.com>
2026-01-19 02:00:59 +0530

    build: Add pako alias to Vite configuration.

--- vite.config.ts
+++ vite.config.ts
@@ -51,6 +51,7 @@ export default defineConfig({
       '@radix-ui/react-alert-dialog@1.1.6': '@radix-ui/react-alert-dialog',
       '@radix-ui/react-accordion@1.2.3': '@radix-ui/react-accordion',
       '@': path.resolve(__dirname, './src'),
+      'pako/lib/zlib/zstream': 'pako',
     },
   },
   build: {
